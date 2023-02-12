07d98b2fee448d655cb1620186a0fa888f9d1c44
JoanAlexanderLopezMantilla <joan_lopez_77@hotmail.com>
2023-02-12 15:38:15 -0500

    Operadores Aritmeticos

---
+++ main.js
@@ -0,0 +1,28 @@
+let num1= 50, num2 =32;
+let resultado;
+let myStyle = "background: #117911; color: #fcf6f6; font-family: sans-serif; padding: 5px; border: 1px solid #4e82e2; border-radius: 5px;";
+
+//operador de adicion 'suma'
+resultado = num1+num2;
+console.log(`%c${num1} + ${num2} = ${resultado}`, myStyle);
+
+//operador de sustracion 'resta'
+resultado = num1-num2;
+console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+
+//operador de Division 'Division'
+resultado = num1/num2;
+console.log(`%c${num1} / ${num2} = ${resultado}`, myStyle);
+
+//operador de Multiplicacion 'Multiplicacion'
+resultado = num1*num2;
+console.log(`%c${num1} * ${num2} = ${resultado}`, myStyle);
+
+//operador de Exponenciacion 'Exponenciacion'
+resultado = num1**num2;
+console.log(`%c${num1} ** ${num2} = ${resultado}`, myStyle);
+
+//operador de Residuo 'residuo'
+resultado = num1 % num2;
+console.log(`%c${num1} % ${num2} = ${resultado}`, myStyle);
+
